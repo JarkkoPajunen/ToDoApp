27ec1b8d5cc97fc53590a4799556416c99595e20
dr0w <dr0w@LAPTOP-MIRUQ5RS>
2020-12-02 10:17:47 +0200

    Säädetty lisää juttuja

--- ToDoApp/src/Task.java
+++ ToDoApp/src/Task.java
@@ -5,6 +5,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Task extends JFrame {
 
@@ -12,6 +17,14 @@ public class Task extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JTextPane txtpnCreateATask;
+	private JTextPane txtpnTask;
+	private JTextPane txtpnTask_1;
+	private JTextPane txtpnTask_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -34,25 +47,84 @@ public class Task extends JFrame {
 	 */
 	public Task() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 595, 280);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(32, 40, 239, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
 		textField_1 = new JTextField();
-		textField_1.setBounds(32, 88, 239, 20);
+		textField_1.setBounds(91, 40, 355, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(32, 132, 239, 20);
+		textField_2.setBounds(91, 88, 355, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
+		
+		btnNewButton = new JButton("Create");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+//				ToDoApp.getTextField().setText(textField.getText());
+				
+				
+			}
+		});
+		btnNewButton.setBounds(480, 39, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Create");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+//+				Task.textField_1.setText(textField_1.getText());
+				
+			}
+		});
+		btnNewButton_1.setBounds(480, 87, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("Create");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+//				Task.textField_2.setText(textField_2.getText());
+				
+			}
+		});
+		btnNewButton_2.setBounds(480, 131, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		txtpnCreateATask = new JTextPane();
+		txtpnCreateATask.setBackground(Color.WHITE);
+		txtpnCreateATask.setText("Create a task");
+		txtpnCreateATask.setBounds(177, 0, 80, 20);
+		contentPane.add(txtpnCreateATask);
+		
+		txtpnTask = new JTextPane();
+		txtpnTask.setEditable(false);
+		txtpnTask.setText("Task 1");
+		txtpnTask.setBounds(21, 40, 46, 20);
+		contentPane.add(txtpnTask);
+		
+		txtpnTask_1 = new JTextPane();
+		txtpnTask_1.setEditable(false);
+		txtpnTask_1.setText("Task 2");
+		txtpnTask_1.setBounds(21, 88, 46, 20);
+		contentPane.add(txtpnTask_1);
+		
+		txtpnTask_2 = new JTextPane();
+		txtpnTask_2.setEditable(false);
+		txtpnTask_2.setText("Task 3");
+		txtpnTask_2.setBounds(21, 132, 46, 20);
+		contentPane.add(txtpnTask_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(91, 132, 355, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
 	}
 }
